2221fd66102f8fe367228d73302e6f475fe229f8
Rupal Mahajan <maharup@amazon.com>
2023-02-02 08:07:25 +0000

    Add const for test url and credentials
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>

--- test/tenant.test.js
+++ test/tenant.test.js
@@ -3,14 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const cli = require('./cli.js');
+const { url, credentials, cli } = require('./cli.js');
 const fs = require('fs');
 jest.useRealTimers();
 
 describe('tenant option', () => {
 
     test('dashboard report with private tenant', async () => {
-        let result = await cli(['-u', 'http://localhost:5601/app/dashboards#/view/722b74f0-b882-11e8-a6d9-e546fe2bba5f', '-a', 'basic', '-c', 'admin:admin',
+        let result = await cli(['-u', url + '/app/dashboards#/view/722b74f0-b882-11e8-a6d9-e546fe2bba5f', '-a', 'basic', '-c', credentials,
             '-n', 'privatetenantdashboard', '-t', 'private'], '.');
         expect(result.code).toBe(0);
         const expectedFile = './privatetenantdashboard.pdf';
@@ -20,7 +20,7 @@ describe('tenant option', () => {
     }, 150000);
 
     test('dashboard report with global tenant', async () => {
-        let result = await cli(['-u', 'http://localhost:5601/app/dashboards#/view/edf84fe0-e1a0-11e7-b6d5-4dc382ef7f5b', '-a', 'basic', '-c', 'admin:admin',
+        let result = await cli(['-u', `${url}/app/dashboards#/view/edf84fe0-e1a0-11e7-b6d5-4dc382ef7f5b`, '-a', 'basic', '-c', credentials,
             '-n', 'globaltenantdashboard', '-t', 'global'], '.');
         expect(result.code).toBe(0);
         const expectedFile = './globaltenantdashboard.pdf';
@@ -30,7 +30,7 @@ describe('tenant option', () => {
     }, 150000);
 
     test('dashboard report with custom tenant', async () => {
-        let result = await cli(['-u', 'http://localhost:5601/app/dashboards#/view/7adfa750-4c81-11e8-b3d7-01146121b73d', '-a', 'basic', '-c', 'admin:admin',
+        let result = await cli(['-u', `${url}/app/dashboards#/view/7adfa750-4c81-11e8-b3d7-01146121b73d`, '-a', 'basic', '-c', credentials,
             '-n', 'customtenantdashboard', '-t', 'admin_tenant'], '.');
         expect(result.code).toBe(0);
         const expectedFile = './customtenantdashboard.pdf';
